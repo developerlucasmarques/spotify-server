071065920863e9565e65fa45f04124eaa8cae830
mlucasdev <lucasmsantos819@gmail.com>
2022-06-22 13:05:55 -0300

    feat: implement config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Spotify server')
+    .setDescription('Aplicação que simula algumas funcionalidades do Spotify.')
+    .setVersion('0.0.0')
+    .addTag('Manager-Admin')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3333);
 }
 bootstrap();
