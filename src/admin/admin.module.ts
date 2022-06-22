3e1317852abdc6b4fe5aa6c92974805851fae7f5
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-22 18:26:24 -0300

    fix(admin): change name module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -8,4 +8,4 @@ import { AdminService } from './admin.service';
   controllers: [AdminController],
   providers: [AdminService],
 })
-export class ManagerAdminModule {}
+export class AdminModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ManagerAdminModule } from './admin/admin.module';
+import { AdminModule } from './admin/admin.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, ManagerAdminModule],
+  imports: [PrismaModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
