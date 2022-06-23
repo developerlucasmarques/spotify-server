f184889c2a9ab51177a05f2413c30084d56afdbe
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-23 17:08:40 -0300

    Revert "feat(admin): implement `update`

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -8,15 +8,11 @@ import {
   Param,
   Patch,
   Post,
-  UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Admin } from '@prisma/client';
-import { LoggedAdmin } from 'src/auth/logged-admin.decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
-import { UpdateAdminDto } from './dto/update-managerAdmin.dto';
+import { Admin } from './entities/admin.entity';
 
 @Controller('admin')
 @ApiTags('Manager-Admin')
@@ -49,15 +45,14 @@ export class AdminController {
     return this.adminService.findOne(id);
   }
 
-  @Patch()
-  @UseGuards(AuthGuard())
-  @ApiBearerAuth()
-  @ApiOperation({
-    summary: 'Editar nome e senha do Admin.',
-  })
-  update(@LoggedAdmin() admin: Admin, @Body() dto: UpdateAdminDto) {
-    return this.adminService.update(admin.id, dto);
-  }
+  // @Patch(':id')
+  // @ApiOperation({
+  //   summary:
+  //     'Editar a senha do Admin pelo ID',
+  // })
+  // update(@Param('id') id: string) {
+  //   return this.adminService.update();
+  // }
 
   @Delete(':id')
   @ApiOperation({
