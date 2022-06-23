d42e7bab15275715af764772a09ee80df9da3862
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-22 22:05:08 -0300

    fix: NestExpressApplication

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
