8623dc17c7411c2830d99b414cce5cc8ed06f98f
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-25 16:10:49 -0300

    feat: country implement `create`

--- src/country/country,controller.ts
+++ src/country/country,controller.ts
@@ -1,9 +1,20 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Admin } from 'src/admin/entities/admin.entity';
+import { LoggedAdmin } from 'src/auth/logged-admin.decorator';
 import { CountryService } from './country.service';
+import { CreateCountryDto } from './dto/create-country.dto';
 
 @ApiTags('country')
 @Controller()
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Create a new country',
+  })
+  create(@LoggedAdmin() admin: Admin, @Body() dto: CreateCountryDto) {
+    return this.countryService.create(dto);
+  }
 }
--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/handle-error.util';
+import { CreateCountryDto } from './dto/create-country.dto';
+import { Country } from './entities/country,entity';
 
 @Injectable()
 export class CountryService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreateCountryDto) {
+    const data: Country = { ...dto };
+
+    return await this.prisma.country
+      .create({
+        data,
+        select: {
+          id: true,
+          name: true,
+        },
+      })
+      .catch(handleError);
+  }
+
   
 }
