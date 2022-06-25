98462ccccc8249d0ad43c59867d464f654df6998
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-25 16:31:24 -0300

    feat(country): implement UseGuards

--- src/country/country,controller.ts
+++ src/country/country,controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Admin } from 'src/admin/entities/admin.entity';
 import { LoggedAdmin } from 'src/auth/logged-admin.decorator';
 import { CountryService } from './country.service';
@@ -7,6 +8,8 @@ import { CreateCountryDto } from './dto/create-country.dto';
 
 @ApiTags('country')
 @Controller()
+@UseGuards(AuthGuard())
+@ApiBearerAuth()
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
 
--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CountryController } from './country,controller';
 import { CountryService } from './country.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [CountryController],
   providers: [CountryService],
 })
