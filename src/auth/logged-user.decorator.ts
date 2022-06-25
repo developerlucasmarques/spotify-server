a93c56caa81722f23a75cffcc3630d5dbeda0e88
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-25 20:28:31 -0300

    feat(user): implement verify user category

--- src/auth/logged-user.decorator.ts
+++ src/auth/logged-user.decorator.ts
@@ -1,8 +1,18 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 export const LoggedUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   const user = request.user;
+
+  if (user.userCategoryName !== 'user') {
+    throw new UnauthorizedException(
+      'User does not have permission to access this route',
+    );
+  }
 
   delete user.password;
 
