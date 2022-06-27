7e535ae1fe72bdec1d70a689cf2017c8dd0a9667
Gustavo Martins <gugumartin77@gmail.com>
2022-06-27 19:56:24 -0300

    feat(music): complete CRUD with findOne, find by logged in artist, update, and delete

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   UseGuards,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { MusicService } from './music.service';
 import { CreateMusicDto } from './dto/create-music.dto';
@@ -15,6 +17,8 @@ import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoggedArtist } from 'src/auth/logged-artist.decorator';
 import { Artist } from 'src/artist/entities/artist.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/user/entities/user.entity';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
 
 @ApiTags('music')
 @UseGuards(AuthGuard())
@@ -26,29 +30,54 @@ export class MusicController {
   @Post()
   @ApiOperation({
     summary:
-      'Create an music and associate it with the artist who created it (artist)',
+      'Create an music and associate it with the artist who created it (artist) and album.',
   })
   create(@LoggedArtist() artist: Artist, @Body() dto: CreateMusicDto) {
     return this.musicService.create(artist.id, dto);
   }
 
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary: 'List all musics.',
+  })
+  findAll(@LoggedUser() user: User) {
     return this.musicService.findAll();
   }
 
+  @Get('/find-all-musics-artist:id')
+  @ApiOperation({
+    summary: 'Fetch all musics of the artist who is logged in (artist).',
+  })
+  findMusicsArtist(@LoggedArtist() artist: Artist) {
+    return this.musicService.findMusicsArtist(artist.id);
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.musicService.findOne(+id);
+  @ApiOperation({
+    summary: 'View a music by Id',
+  })
+  findOne(@LoggedUser() user: User, @Param('id') musicId: string) {
+    return this.musicService.findOne(musicId);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMusicDto: UpdateMusicDto) {
-    return this.musicService.update(+id, updateMusicDto);
+  @ApiOperation({
+    summary: 'Edit an music of the artist who is logged in (artist)',
+  })
+  update(
+    @LoggedArtist() artist: Artist,
+    @Param('id') musicId: string,
+    @Body() dto: UpdateMusicDto,
+  ) {
+    return this.musicService.update(artist.id, musicId, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.musicService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remove a music by Id',
+  })
+  delete(@LoggedArtist() artist: Artist, @Param('id') musicId: string) {
+    return this.musicService.delete(artist.id, musicId);
   }
 }
