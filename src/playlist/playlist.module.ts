55f749defe61b1a502fd1371382bebd6a6207cfb
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-06-28 21:20:27 -0300

    feat(playlist): import PrismaModule

--- src/playlist/playlist.module.ts
+++ src/playlist/playlist.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlaylistService } from './playlist.service';
 import { PlaylistController } from './playlist.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PlaylistController],
-  providers: [PlaylistService]
+  providers: [PlaylistService],
 })
 export class PlaylistModule {}
