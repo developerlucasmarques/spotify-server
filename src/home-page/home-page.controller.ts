76ff5fc25c631920a67d318587979df002daf1f7
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-05 17:00:26 -0300

    feat(home-page): implement `homePage`

--- src/home-page/home-page.controller.ts
+++ src/home-page/home-page.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserProfileId } from 'src/auth/dto/logged-profile-type';
 import { LoggedUser } from 'src/auth/logged-user.decorator';
 import { HomePageService } from './home-page.service';
 
-@Controller('home-page')
+@ApiTags('home-page')
+@Controller('/')
 export class HomePageController {
   constructor(private readonly homePageService: HomePageService) {}
 
   @Get()
-  homePage(@LoggedUser() userProfileId: UserProfileId) {
-    return this.homePageService.homePage(userProfileId);
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  homePage(@LoggedUser() userProfile: UserProfileId) {
+    return this.homePageService.homePage(
+      userProfile.user.id,
+      userProfile.profileId,
+    );
   }
 }
--- src/home-page/home-page.service.ts
+++ src/home-page/home-page.service.ts
@@ -1,12 +1,82 @@
-import { Get, Injectable } from '@nestjs/common';
-import { UserProfileId } from 'src/auth/dto/logged-profile-type';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/handle-error.util';
 
 @Injectable()
 export class HomePageService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async homePage(userProfileId: UserProfileId) {
-    
+  async homePage(userId: string, profileId: string) {
+    await this.findOneProfileInUser(userId, profileId);
+
+    const playlists = await this.prisma.profile
+      .findUnique({
+        where: { id: profileId },
+        select: {
+          playlists: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+              _count: {
+                select: {
+                  songs: true,
+                },
+              },
+            },
+          },
+          favoritePlaylists: {
+            select: {
+              playlist: {
+                select: {
+                  id: true,
+                  name: true,
+                  image: true,
+                  _count: {
+                    select: {
+                      songs: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+          songs: {
+            select: {
+              song: {
+                select: {
+                  id: true,
+                  name: true,
+                  songUrl: true,
+                },
+              },
+            },
+            take: 10,
+            skip: 0,
+          },
+        },
+      })
+      .catch(handleError);
+
+    return playlists;
+  }
+
+  async findOneProfileInUser(userId: string, profileId: string) {
+    const record = await this.prisma.user
+      .findUnique({
+        where: { id: userId },
+        select: {
+          profiles: {
+            where: {
+              id: profileId,
+            },
+          },
+        },
+      })
+      .catch(handleError);
+
+    if (record.profiles.length === 0) {
+      throw new NotFoundException(`Profile with ID '${profileId}' not found`);
+    }
   }
 }
