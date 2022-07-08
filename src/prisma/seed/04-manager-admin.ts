7490751d3ef69f50cbf838c0d74bab17a8aaa99f
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-07 22:46:14 -0300

    feat(seed): implement manager-admin

---
+++ src/prisma/seed/04-manager-admin.ts
@@ -0,0 +1,38 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+export const admins: Prisma.AdminCreateInput[] = [
+  {
+    name: 'Manager',
+    cpf: '153.743.513-15',
+    email: 'manager@manager.com',
+    password: 'User#5678@!',
+    userCategory: {
+      connect: {
+        name: 'manager',
+      },
+    },
+  },
+  {
+    name: 'Admin',
+    cpf: '713.443.513-15',
+    email: 'admin@admin.com',
+    password: 'User#5678@!',
+    userCategory: {
+      connect: {
+        name: 'admin',
+      },
+    },
+  },
+];
+
+export const admin = async (prisma: PrismaClient) => {
+  for (const obj of Object.values(admins)) {
+    await prisma.admin.upsert({
+      where: { cpf: obj.cpf },
+      update: {},
+      create: {
+        ...obj,
+      },
+    });
+  }
+};
