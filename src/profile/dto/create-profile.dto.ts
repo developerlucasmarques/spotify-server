c13847c3eab9a2a5dab93c183c66b86ab791410c
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-15 17:52:24 -0300

    feat(profile): add `@Length`

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
 
 export class CreateProfileDto {
   @IsNotEmpty()
   @IsString()
+  @Length(1, 20)
   @ApiProperty({
     description: 'Profile Name',
     example: 'Barack Obama',
@@ -12,6 +13,7 @@ export class CreateProfileDto {
 
   @IsNotEmpty()
   @IsUrl()
+  @Length(5, 100)
   @ApiProperty({
     description: 'Profile picture URL',
     example: 'https://profileimage.jpg',
