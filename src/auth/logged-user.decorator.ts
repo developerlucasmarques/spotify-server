2ce3470ca88a3058a43905da86e0eb071a7928e2
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-15 20:20:51 -0300

    feat(auth): implement type User

--- src/auth/logged-user.decorator.ts
+++ src/auth/logged-user.decorator.ts
@@ -3,10 +3,11 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { UserProfileId } from './dto/logged-profile-type';
 
 export const LoggedUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  const userObject = request.user;
+  const userObject: UserProfileId = request.user;
 
   if (userObject.user.userCategoryName !== 'user') {
     throw new UnauthorizedException(
