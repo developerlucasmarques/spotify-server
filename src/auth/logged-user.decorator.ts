d3a578e974ba0b66b11acf5fb4c810aae02a83e2
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-18 17:23:42 -0300

    fix(auth): correction in validation

--- src/auth/logged-user.decorator.ts
+++ src/auth/logged-user.decorator.ts
@@ -3,19 +3,28 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
-import { UserProfileId } from './dto/logged-profile-type';
 
 export const LoggedUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  const userObject: UserProfileId = request.user;
+  const userObject = request.user;
 
-  if (userObject.user.userCategoryName !== 'user') {
+  if (
+    userObject.userCategoryName === 'artist' ||
+    userObject.userCategoryName === 'admin' ||
+    userObject.userCategoryName === 'manager'
+  ) {
     throw new UnauthorizedException(
       'User does not have permission to access this route',
     );
   }
 
-  delete userObject.user.password;
+  if (userObject.user.userCategoryName === 'user') {
+    delete userObject.user.password;
 
-  return userObject;
+    return userObject;
+  } else {
+    throw new UnauthorizedException(
+      'User does not have permission to access this route',
+    );
+  }
 });
