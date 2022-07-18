2477a4bfdc23d77a3a503f2c7ff4190790ac0f41
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-18 17:05:55 -0300

    feat(song): implement `@Length` and `@db.VarChar`

--- src/song/dto/create-song.dto.ts
+++ src/song/dto/create-song.dto.ts
@@ -1,33 +1,35 @@
-import { ApiProperty } from "@nestjs/swagger"
-import { IsNotEmpty, IsString, IsUrl, IsUUID } from "class-validator"
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, IsUUID, Length, MaxLength } from 'class-validator';
 
 export class CreateSongDto {
   @IsString()
-  @IsNotEmpty()
+  @Length(1, 50)
   @ApiProperty({
     description: 'The name of the song.',
     example: 'Right Next Door to Hell',
   })
-  name: string
+  name: string;
 
   @IsUrl()
+  @MaxLength(2000)
   @ApiProperty({
     description: 'The url of the song.',
     example: 'https://youtu.be/Zqo7j2yFoFs',
   })
-  songUrl: string
+  songUrl: string;
 
   @IsUUID()
   @ApiProperty({
     description: 'The album of the song.',
     example: '0eee08d7-8a4b-4751-bdfe-1f0bd61a6987',
   })
-  albumId: string
+  albumId: string;
 
-  @IsUUID(undefined, {each: true})
+  @IsUUID(undefined, { each: true })
   @ApiProperty({
     description: 'Category id to create the relationship with song',
-    example: '["76b73966-928c-407e-8c35-ba327f4d200c", "0eee08d7-8a4b-4751-bdfe-1f0bd61a6987"]',
+    example:
+      '["76b73966-928c-407e-8c35-ba327f4d200c", "0eee08d7-8a4b-4751-bdfe-1f0bd61a6987"]',
   })
-  categoryId: string[]
+  categoryId: string[];
 }
