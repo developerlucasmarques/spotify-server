d603033aa7480f30ef4b4e80cf3009d94ad259bb
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-07-18 18:07:09 -0300

    feat(song): remove albumId and categoryId

--- src/song/dto/update-song.dto.ts
+++ src/song/dto/update-song.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSongDto } from './create-song.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, Length, MaxLength } from 'class-validator';
 
-export class UpdateSongDto extends PartialType(CreateSongDto) {}
+export class UpdateSongDto {
+  @IsString()
+  @Length(1, 50)
+  @ApiProperty({
+    description: 'The name of the song.',
+    example: 'Right Next Door to Hell',
+  })
+  name: string;
+
+  @IsUrl()
+  @MaxLength(2000)
+  @ApiProperty({
+    description: 'The url of the song.',
+    example: 'https://youtu.be/Zqo7j2yFoFs',
+  })
+  songUrl: string;
+}
